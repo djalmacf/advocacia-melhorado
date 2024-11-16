4d89e5ba73bf34199cced1ab898c7fa4d08cd794
djalmacf <djalmacf@hotmail.com>
2024-11-15 22:45:35 -0300

    att

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-AORD2DW3.mjs";import"./chunk-TV6JPXMU.mjs";import"./chunk-UGFBNU6G.mjs";export{a as default};
+//# sourceMappingURL=main.server.mjs.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqComponent } from './faq/faq.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FooterComponent, FaqComponent, HeaderComponent, CarouselComponent, ContactFormComponent, AboutUsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'advocacia-melhorado';
-}
+export class AppComponent {}
